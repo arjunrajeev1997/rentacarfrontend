9c7bdb483b09fce4923845817ae8760ba107ddee
arjunrajeev1997 <arjun.rajeev327@gmail.com>
2024-02-08 18:06:16 +0530

    latest

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -38,16 +38,17 @@ function Home() {
       <section className='about'>
       <Row className='p-5'>
         <Col className='md-4 lg-4 '>
-          <Link className='text-decoration-none' to={'/display'}>
+          <Link className='text-decoration-none' to={'/cars'}>
           <Card data-aos="zoom-in" data-aos-duration="1000" className='' style={{ width: '23.1rem', height:'30rem' }}>
             <Card.Img className='cardimg' variant="top" style={{width:'23rem',height:'18rem'}} src="https://i.postimg.cc/RZWySkRP/qnpasplo-updated-2023-rollsroyce-phantom-series-ii-unveiled-with-new-features-and-connected-tech-625.webp" />
             <Card.Body>
-              <Link style={{textDecoration:'none'}} to={'/cars'}>
+              
               <Card.Title className='text-dark fw-bolder fs-4'>LUXURY CARS</Card.Title>
+              
               <Card.Text>
                 We make your favourite place in the world more beautiful.Get ready to build a lifelong fav hanging spot
               </Card.Text>
-              </Link>
+              
             </Card.Body>
           </Card>
           </Link>
@@ -56,16 +57,17 @@ function Home() {
 
 
         <Col className='md-4 lg-4' style={{paddingTop:'70px'}}>
-        <Link className='text-decoration-none' to={'/display'}>
+        <Link className='text-decoration-none' to={'/cars'}>
           <Card data-aos="zoom-in" data-aos-duration="1500" className='' style={{ width: '23.1rem', height:'30rem' }}>
             <Card.Img className='cardimg'  variant="top" style={{width:'23rem',height:'18rem'}} src="https://i.postimg.cc/9F7yc19d/profile1705575369.avif" />
             <Card.Body>
-            <Link style={{textDecoration:'none'}} to={'/cars'}>
+           
               <Card.Title className='text-dark fw-bolder fs-4'>ELECTRIC CARS</Card.Title>
+             
               <Card.Text>
               We make your favourite place in the world more beautiful.Get ready to build a lifelong fav hanging spot
               </Card.Text>
-              </Link>
+              
             </Card.Body>
           </Card>
           </Link>
@@ -74,16 +76,17 @@ function Home() {
 
 
         <Col className='md-4 lg-4' style={{paddingTop:'140px'}}>
-          <Link className='text-decoration-none' to={'/display'}>
+          <Link className='text-decoration-none' to={'/cars'}>
           <Card data-aos="zoom-in" data-aos-duration="2000" className='crd' style={{ width: '24rem', height:'29rem' }}>
             <Card.Img className='cardimg' variant="top" style={{width:'24rem',height:'16rem'}} src="https://i.postimg.cc/zfXjDK8q/honda-city-new-1680237436647-1684985160792.avif" />
             <Card.Body>
-            <Link style={{textDecoration:'none'}} to={'/cars'}>
+           
               <Card.Title className='text-dark fw-bolder fs-4'>BUDGET CARS</Card.Title>
+             
               <Card.Text className='text-black'>
               We make your favourite place in the world more beautiful.Get ready to build a lifelong fav hanging spot
               </Card.Text>
-              </Link>
+              
             </Card.Body>
           </Card>
           </Link>
